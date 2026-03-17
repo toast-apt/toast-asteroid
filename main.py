119faed3aab6db64b34a770d0ae9418243817905
Andrew <185149126+toast-apt@users.noreply.github.com>
2026-03-17 19:58:52 -0400

    Updated draw method and added groups.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from player import Player
 import pygame
+from player import Player
 from constants import *
 from logger import log_state
 
@@ -13,6 +13,10 @@ def main():
     dt = 0
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    updatable = pygame.sprite.Group() 
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     
     while True:
@@ -21,9 +25,10 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
-        player.update(dt)
+        updatable.update(dt)
         screen.fill("black")  # Clear the screen with black
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
         pygame.display.flip()
         clock.tick(60)  # Limit to 60 frames per second
         dt = clock.get_time() / 1000.0  # Convert milliseconds to seconds
